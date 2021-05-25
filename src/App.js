ee73803b8c2b4d78934543302f6e39633b861cfc
abhishek <abhishekmazumder69@gmail.com>
2021-05-25 20:02:00 +0530

    adding product to cart implemented

--- src/App.js
+++ src/App.js
@@ -5,22 +5,31 @@ import { Products, NavBar } from './components';
 
 const App = () => {
 	const [products, setProducts] = useState([]);
+	const [cart, setCart] = useState({});
 
 	const fetchProducts = async () => {
 		const { data } = await commerce.products.list();
 		setProducts(data);
 	};
 
+	const fetchCart = async () => {
+		setCart(await commerce.cart.retrieve());
+	};
+
+	const handleAddToCart = async (productId, qty) => {
+		const item = await commerce.cart.add(productId, qty);
+		setCart(item.cart);
+	};
+	console.log(cart);
 	useEffect(() => {
 		fetchProducts();
+		fetchCart();
 	}, []);
-
-	console.log(products);
 
 	return (
 		<div>
-			<NavBar />
-			<Products products={products} />
+			<NavBar totalItems={cart.total_items} />
+			<Products products={products} onAddToCart={handleAddToCart} />
 		</div>
 	);
 };
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,7 +13,7 @@ import logo from '../../assets/logo.png';
 
 import useStyles from './styles';
 
-const NavBar = () => {
+const NavBar = ({totalItems}) => {
 	const classes = useStyles();
 	return (
 		<>
@@ -26,7 +26,7 @@ const NavBar = () => {
 					<div className={classes.grow} />
 					<div className={classes.button}>
 						<IconButton aria-label="show cart item" color="inherit">
-							<Badge badgeContent={2} color="secondary">
+							<Badge badgeContent={totalItems} color="secondary">
 								<ShoppingCart />
 							</Badge>
 						</IconButton>
